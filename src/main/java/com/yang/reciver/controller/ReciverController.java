d6a2dc39b7ecce6b2cef9d2bfed9a21577f50349
admin <65307023@qq.com>
2018-01-24 17:29:36 +0800

    成功回复用户发送的消息

--- src/main/java/com/yang/reciver/controller/ReciverController.java
+++ src/main/java/com/yang/reciver/controller/ReciverController.java
@@ -1,15 +1,20 @@
 package com.yang.reciver.controller;
 
+import com.yang.reciver.model.BaseMessage;
 import com.yang.reciver.service.ReciverSerivce;
+import com.yang.util.XmlManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 /**
  * 监测数据表配置表 controller
@@ -25,7 +30,31 @@ public class ReciverController {
     @RequestMapping("/verify")
     @ResponseBody
     public String saas(HttpServletRequest request) throws Exception{
-        InputStream in=request.getInputStream();
-        return reciverSerivce.verify(request);
+        String echostr = request.getParameter("echostr");
+        if (echostr!=null) return reciverSerivce.verify(request);
+
+        return dealMessage(request);
+    }
+
+    public String dealMessage(HttpServletRequest request){
+        System.out.println("==============================");
+        Map<String, String[]> map = request.getParameterMap();
+        for(String key:map.keySet())
+            System.out.println("key:"+key+";value:"+map.get(key)[0]);
+        String resulet="";
+        try {
+            ServletInputStream inputStream = request.getInputStream();
+            BaseMessage baseMessage=(BaseMessage) XmlManager.parserXml(inputStream, BaseMessage.class);
+            inputStream.close();
+            String toUserName = baseMessage.getToUserName();
+            String fromUserName = baseMessage.getFromUserName();
+            baseMessage.setToUserName(fromUserName);
+            baseMessage.setFromUserName(toUserName);
+            resulet = XmlManager.buildXml(baseMessage);
+            System.out.println("---------------->"+resulet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resulet;
     }
 }
---
+++ src/main/java/com/yang/reciver/model/BaseMessage.java
@@ -0,0 +1,79 @@
+package com.yang.reciver.model;
+
+/**
+ * Created by Administrator on 2018/1/24.
+ */
+public class BaseMessage {
+    private String  toUserName;
+    private String  fromUserName;
+    private String  createTime;
+    private String  msgType;// text:文字 ;image:图片
+    private String  content;
+    private String  picUrl;
+    private String  mediaId;
+    private String  msgId;
+
+    public String getToUserName() {
+        return toUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        this.toUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+}
