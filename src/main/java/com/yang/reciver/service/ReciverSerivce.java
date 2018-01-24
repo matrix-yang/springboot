a700aea40a87b3cce3fc73d194dad0bd88190ea1
admin <65307023@qq.com>
2018-01-24 11:56:31 +0800

    微信验证服务器

--- src/main/java/com/yang/reciver/service/ReciverSerivce.java
+++ src/main/java/com/yang/reciver/service/ReciverSerivce.java
@@ -2,8 +2,6 @@ package com.yang.reciver.service;
 
 
 import com.yang.reciver.SignUtil;
-import com.yang.util.web.HttpClientUtil;
-import net.sf.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,23 +26,5 @@ public class ReciverSerivce {
             return code;
         }
         return code;
-    }
-
-    public String getCode(HttpServletRequest request) {
-
-        String code = request.getParameter("code");
-        String url= request.getParameter("state");
-        String openid=getFreshToken(code);
-        return url+"?openid="+openid;
-    }
-
-    public String getFreshToken(String code){
-        // 刷新token
-
-        String r = HttpClientUtil.sendGet("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx890cd6bd6b2e7c8d&secret=e336c8ea549e6cffc4789f0ffeae20dd&code="+code+"&grant_type=authorization_code","utf-8","utf-8");
-        JSONObject object = JSONObject.fromObject(r);
-        String web_access_token = (String) object.get("access_token");
-        String openid=(String) object.get("openid");
-        return openid;
     }
 }
